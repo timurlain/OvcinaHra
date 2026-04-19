24e64cbec003c56d383c544da24da405607fb93e
Tomáš Pajonk <tomas.pajonk@solvertech.cz>
2026-04-19 06:49:15 +0200

    feat(skills): add Skill EF Core configuration [v0.3.0-dev]

---
+++ src/OvcinaHra.Api/Data/Configurations/SkillConfiguration.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OvcinaHra.Shared.Domain.Entities;
+
+namespace OvcinaHra.Api.Data.Configurations;
+
+public class SkillConfiguration : IEntityTypeConfiguration<Skill>
+{
+    public void Configure(EntityTypeBuilder<Skill> builder)
+    {
+        builder.HasKey(e => e.Id);
+        builder.Property(e => e.Name).IsRequired().HasMaxLength(100);
+        builder.HasIndex(e => e.Name).IsUnique();
+        builder.Property(e => e.ClassRestriction).HasConversion<string>().HasMaxLength(20);
+        builder.Property(e => e.Effect).HasMaxLength(1000);
+        builder.Property(e => e.RequirementNotes).HasMaxLength(1000);
+        builder.Property(e => e.ImagePath).HasMaxLength(500);
+    }
+}
